5dd7519783b14d70d19f561e739637c80fc41faf
cainiao <2042298875@qq.com>
2018-06-28 17:37:13 +0800

    2018-06-28

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ def upload(path):
     gcom = subprocess.run(["git", "commit", "-m" + date])
     grm = subprocess.run(["git","remote","rm","origin"])
     gremote = subprocess.run(["git", "remote", "add", "origin", "https://github.com/zhazhahui789/111.git"])
-    gpull = subprocess.run(["git","pull","origin","master"])
+    gpull = subprocess.run(["git","pull","origin","master","----allow-unrelated-histories"])
     gpush = subprocess.run(["git", "push", "-u", "origin", "master"])
 
 
