9e2e93ba847414eda525cdccbef66e54e5652bb8
cainiao <2042298875@qq.com>
2018-06-28 17:31:07 +0800

    2018-06-28

--- test.py
+++ test.py
@@ -7,9 +7,9 @@ def upload(path):
     date = datetime.datetime.today().isoformat()[0:10]
     init = subprocess.run(["git", "init",path])
     gcren = subprocess.run(["git", "config", "--global", "credential.helper", "store"])
-    path1 = path+"\\"
     gadd = subprocess.run(["git", "add","." ])
     gcom = subprocess.run(["git", "commit", "-m" + date])
+    grm = subprocess.run(["git","remote","rm","origin"])
     gremote = subprocess.run(["git", "remote", "add", "origin", "https://github.com/zhazhahui789/111.git"])
     gpush = subprocess.run(["git", "push", "-u", "origin", "master"])
 
